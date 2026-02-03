c0d6b6aaccb0856b2a7686fcf503094fbe80cdc2
Tonny Blair <jusanotherthursday@gmail.com>
2026-02-03 19:29:53 +0300

    added database connection logic

---
+++ blogSystem/backend/db_logic.php
@@ -0,0 +1,29 @@
+<?php 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+$host = "localhost"; // where MySQL is running
+$db = "BLOG";        // database name in MySQL
+$user = "root";      // MySQL username
+$pass = "";          // password for MySQL
+
+// ----- CREATING THE CONNECTION -----
+try{
+    // pdo -> php data objects
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$db;charset=utf8", // DSN(Data Source Name)
+        $user,
+        $pass
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $pdo->query("SELECT 1");
+    echo "<script>console.log('DB connected');</script>";
+    echo "DB connected";
+    exit;
+} catch (PDOException $e){
+    die("Database connection failed :(".$e->getMessage());
+}
+?>
