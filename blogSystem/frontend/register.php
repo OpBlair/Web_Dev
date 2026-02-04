32facc21d126cfacc0eace13c6abbbb4a38d75c5
Tonny Blair <jusanotherthursday@gmail.com>
2026-02-04 23:27:49 +0300

    working on registration page

---
+++ blogSystem/frontend/register.php
@@ -0,0 +1,25 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register | Blog</title>
+</head>
+<body>
+    <div class="register">
+        <h2>Create Account</h2>
+        <form action="" method="post" style="display:flex; flex-direction:column; gap:15px;">
+            <input type="text" placeholder="first name">
+            <input type="text" placeholder="last name">
+            <input type="email" placeholder="email">
+            <input type="password" placeholder="enter new password">
+            <input type="password" placeholder="confirm password">
+
+            <button type="submit">Register</button>
+        </form>
+        <p style="font-size: 0.8rem;">Already have an account ? <a href="login.php">Login</a></p>
+    </div>
+</body>
+</html>
