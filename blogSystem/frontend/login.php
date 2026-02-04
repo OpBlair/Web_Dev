08dca46d27398ab0ac14ba1cc9bd9c0efa42d889
Tonny Blair <jusanotherthursday@gmail.com>
2026-02-04 23:28:22 +0300

    added a login page

---
+++ blogSystem/frontend/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog | Login</title>
+</head>
+<body>
+    <div class="login">
+        <h2>Login</h2>
+        <form action="" method="post" style="display:flex; flex-direction:column; gap:15px;">
+            <input type="email" placeholder="email">
+            <input type="password" placeholder="enter password">
+
+            <button type="submit">Login</button>
+        </form>
+        <p style="font-size: 0.8rem;">don't have an account ? <a href="register.php">Register</a></p>
+    </div>
+</body>
+</html>
