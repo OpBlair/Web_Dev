2493147859cedae46baceca12d90b97f1c110ec9
opblair <jusanotherthursday@gmail.com>
2026-07-13 15:50:40 +0300

    feat: added layout styles.

--- results-summary-component/script.js
+++ results-summary-component/script.js
@@ -2,8 +2,6 @@
 
 const resultSummary = document.getElementById('summary-items-list');
 
-if(!resultSummary) {console.log("aye aye");}
-
 async function loadResultSummaryData(){
     try{
         const response = await fetch('./data.json');
@@ -12,12 +10,13 @@ async function loadResultSummaryData(){
             throw new Error("There was an error fetching the data");
         } else{
             const data = await response.json();
-            console.log(data);
-
+            
             data.forEach(item => {
                 const itemContainer = document.createElement('div');
                 itemContainer.classList.add('summary-item');
 
+                itemContainer.classList.add(item.category.toLowerCase());
+                
                 itemContainer.innerHTML = `
                 <div class="result-category">
                     <img src="${item.icon}">
