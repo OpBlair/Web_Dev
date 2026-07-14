9cb75b7e70fbc7b6cc479c55ebfdb09a0a289ca2
opblair <jusanotherthursday@gmail.com>
2026-07-14 23:57:29 +0300

    feat: dynamically load product items from json file.

---
+++ product-list/script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const productContainer = document.getElementById('product-list');
+
+async function loadFoodItems(){
+    try{
+        const response = await fetch('./data.json');
+
+        if(!response.ok){
+            throw new Error('There was an error fetching the data');
+        }else{
+            const data = await response.json();
+            
+            data.forEach(item => {
+                const itemContainer = document.createElement('div');
+                itemContainer.classList.add('product-item');
+
+                itemContainer.classList.add(item.category.toLowerCase().replace(/\s+/g, '-'));
+
+                itemContainer.innerHTML = `
+                    <div class="product-image-container">
+                        <picture>
+                            <source media="(min-width: 1024px)" srcset="${item.image.desktop}">
+                            <source media="(min-width: 768px)" srcset="${item.image.tablet}">
+                            <img src="${item.image.mobile}" alt="${item.name}" class="product-image">
+                        </picture>
+                        <button class="add-to-cart-btn" data-name="${item.name}" data-price="${item.price}" data-image="${item.image.thumbnail}">
+                            <img src="./assets/images/icon-add-to-cart.svg" alt="" /> Add to Cart
+                        </button>
+                    </div>
+                    <p class="category">${item.category}</p>
+                    <p class="name">${item.name}</p>
+                    <p class="price">$${item.price.toFixed(2)}</p>        
+                `;
+
+                productContainer.appendChild(itemContainer);
+            });
+        }
+    }catch(error){
+        console.error("An error occured");
+    }
+}
+
+loadFoodItems();
