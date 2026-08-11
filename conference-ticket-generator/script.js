7c28bb31e174375165d93e7476736833e222981f
opblair <jusanotherthursday@gmail.com>
2026-08-12 00:14:45 +0300

    feat: implemented js to handle file upload.

---
+++ conference-ticket-generator/script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const ticketForm = document.getElementById('ticketForm');
+const ticketResult = document.getElementById('ticketResult');
+
+const ticketHeaderTitle = document.getElementById('content-title');
+const ticketHeaderDescription = document.getElementById('content-description');
+
+const displayName = document.getElementById('displayFullName');
+const githubDisplayName = document.getElementById('displayGithub');
+const ticketAvatar = document.getElementById('ticketAvatar');
+const avatarInput = document.getElementById('avatar');
+
+ticketForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const fullName = document.getElementById('fullName').value;
+    const userEmail = document.getElementById('email').value;
+    let githubUsername = document.getElementById('github').value;
+
+    // Handle File Upload Preview
+    if (avatarInput.files && avatarInput.files[0]) {
+        const reader = new FileReader();
+        reader.onload = function (event) {
+            ticketAvatar.src = event.target.result;
+        };
+        reader.readAsDataURL(avatarInput.files[0]);
+    }
+
+    ticketForm.style.display = 'none';
+    ticketResult.style.display = 'flex';
+    ticketResult.removeAttribute('hidden'); 
+
+    ticketHeaderTitle.innerHTML = `Congrats, <span id="ticketName">${fullName}</span>! Your ticket is ready.`;
+    ticketHeaderDescription.innerHTML = `We've emailed your ticket to <span id="ticketEmail">${userEmail}</span> and will send updates in the run up to the event.`;
+
+    displayName.textContent = fullName;
+    
+    // Auto-format GitHub syntax if user omitted the prefix symbol
+    if (!githubUsername.startsWith('@')) {
+        githubUsername = '@' + githubUsername;
+    }
+    githubDisplayName.textContent = `${githubUsername}`;
+
+    const randomNumber = String(Math.floor(Math.random() * 10000)).padStart(5, '0');
+    document.querySelector('.ticket-number').textContent = `#${randomNumber}`;
+});
